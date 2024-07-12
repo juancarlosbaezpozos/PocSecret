87cbe460261d4e14a78314b4852a0bc4cf399329
Carlos Baez <jbaez@outlook.com>
2024-07-12 01:05:20 -0600

    prueba final

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PocSecretToken
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine($"The secret token is {Constants.Secret}");
         }
